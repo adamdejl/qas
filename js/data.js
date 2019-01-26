3bae74183b8974596ce28ed540d1fa4135855ea6
Adam Dejl <adam.dejl18@imperial.ac.uk>
2019-01-26 21:06:15 +0000

    data.js: Outline for born query

--- js/data.js
+++ js/data.js
@@ -70,5 +70,40 @@ var data = {
             }
             GROUP BY ?object ?objectLabel ?objectDescription ?article ?ended ?picture
             ORDER BY DESC(?count)`
+    },
+    2: {
+        type: "whenBorn",
+        patterns: {
+            0: /what was (.+?) born\??$/i,
+            1: /what is the date of birth of (.+?)\??$/i
+        },
+        inputs: {
+            0: "$searchedObject"
+        },
+        query: `
+            SELECT ?object ?objectLabel ?objectDescription ?article ?ended ?picture (COUNT(DISTINCT ?sitelink) AS ?count) WHERE {
+              ?object rdfs:label|skos:altLabel "London"@en .
+              OPTIONAL {
+                ?article schema:isPartOf <https://en.wikipedia.org/>;
+                  schema:about ?object.
+              }
+              OPTIONAL {
+                ?object wdt:P576 ?ended .
+              }
+              OPTIONAL {
+                ?sitelink schema:about ?object .
+              }
+              OPTIONAL {
+                ?object wdt:P18 ?picture .
+              }
+              SERVICE wikibase:label {
+                bd:serviceParam wikibase:language "en" .
+              }
+              FILTER NOT EXISTS{?object wdt:P31 wd:Q5}
+              FILTER NOT EXISTS{?object wdt:P31 wd:Q4167410}
+              FILTER NOT EXISTS{?object wdt:P31/wdt:P279* wd:Q18616576}
+            }
+            GROUP BY ?object ?objectLabel ?objectDescription ?article ?ended ?picture
+            ORDER BY DESC(?count)`
     }
 }
