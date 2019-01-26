e2ad18bccd38813873557fd3a2c1e9eb724af323
Adam Dejl <adam.dejl18@imperial.ac.uk>
2019-01-26 13:45:15 +0000

    query.js: Started work on regex matching

--- query.js
+++ query.js
@@ -1,9 +1,25 @@
+"use strict"
+
 jQuery(function($) {
     $("#querySubmit").click(function() {
         processQuery($("#queryInput").val());
     });
 
+    $(document).keydown(function(key) {
+        if (key.which == 13) {
+            processQuery($("#queryInput").val());
+        }
+    });
+
     function processQuery(query) {
-        console.log(query);
+        var queryMatch;
+        var queryInputs = [];
+        for (var i = 0; i < data.length; i++) {
+            for (var j = 0; j < data[i].patterns; j++) {
+                if (data[i].patterns[j].text(query)) {
+                    
+                }
+            }
+        }
     }
 });
